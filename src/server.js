41ffeb968e3dd5b5eab7b204cb9b757d2e0f5036
Safra-boy <juniorsifronio@gmail.com>
2025-07-30 20:46:10 +0100

    Altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("boas-vinda a API do allbooks")
+  console.log("API disponivel atraves da url em http://localhost:8000")
 })
